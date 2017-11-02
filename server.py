235205e6c148373a1571475db13b7bc3986cefe7
Unix&Me <unixnme@gmail.com>
2017-11-02 20:37:28 +0900

    add index html

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 PORT = '8888'
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Hara'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=PORT)
