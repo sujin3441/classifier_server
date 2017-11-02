7cb62ecdabcf6d5483133e6da05212d7da61155b
Unix&Me <unixnme@gmail.com>
2017-11-02 21:38:07 +0900

    display image from url

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
+import os
+import urllib
+
 app = Flask(__name__)
 
 PORT = '8888'
+UPLOAD = 'uploads'
 
 @app.route('/')
 def index():
@@ -9,8 +13,11 @@ def index():
                            title='ResNet50 Classification Demo')
 
 @app.route('/predict', methods=['POST'])
-def predict():
+def predict_url():
     address = request.form['address']
+    filename = os.path.join(UPLOAD, address.split('/')[-1])
+    with open(filename, 'wb') as f:
+        f.write(urllib.urlopen(address).read())
     return render_template("predict.html", url_address=address)
  
 if __name__ == "__main__":
