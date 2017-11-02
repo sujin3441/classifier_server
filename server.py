85f97081981376c379cf2ccf7c5f3f21c1bf4c26
Unix&Me <unixnme@gmail.com>
2017-11-02 20:54:05 +0900

    implement loop

--- server.py
+++ server.py
@@ -6,9 +6,21 @@ PORT = '8888'
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Hara'}  # fake user
-    return render_template('index.html',
-                           user=user)
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=PORT)
