3d33a82d604452d5162463f7f7bca89d108a6a4d
Unix&Me <unixnme@gmail.com>
2017-11-02 20:26:16 +0900

    visible to all

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
- 
+
+PORT = '8888'
+
 @app.route("/")
 def hello():
     return "Hello World!"
  
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=PORT)
