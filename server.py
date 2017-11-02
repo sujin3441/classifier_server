5be6e1490966ad9059971e5d74dbcad19ee04e62
Unix&Me <unixnme@gmail.com>
2017-11-02 22:28:09 +0900

    successfully classifying!

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, send_from_directory
 import os
 import urllib
+from keras.models import load_model
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
 PORT = '8888'
 UPLOAD = 'uploads'
-
+MODEL = 'resnet50.h5'
 
 @app.route('/')
 def index():
@@ -37,8 +41,22 @@ def serve_files(path):
 
 
 def predict(filename):
-    return render_template('predict.html', filename=filename)
+    img = cv2.imread(filename)
+    img = cv2.resize(img, (224,224))[...,::-1]
+    img = np.expand_dims(img, axis=0).astype(np.float32)
+    x_batch = preprocess_input(img)
+    pred = model.predict(x_batch)
+    decode_result = decode_predictions(pred)[0]
+    result = []
+    for r in decode_result:
+        result.append({'name':r[1], 'prob':r[2]*100})
+
+    return render_template('predict.html',
+                           filename=filename,
+                           predictions=result)
 
 
 if __name__ == "__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    model = load_model(MODEL)
     app.run(host='0.0.0.0', port=PORT)
