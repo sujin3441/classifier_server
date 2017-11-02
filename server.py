0a262a37b1fefbf685d5f9dcba0cdebe45481474
Unix&Me <unixnme@gmail.com>
2017-11-02 21:49:21 +0900

    add option to upload the image

--- server.py
+++ server.py
@@ -19,6 +19,14 @@ def predict_url():
     with open(filename, 'wb') as f:
         f.write(urllib.urlopen(address).read())
     return render_template("predict.html", url_address=address)
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    f = request.files['image']
+    filename = os.path.join(UPLOAD, f.filename)
+    f.save(filename)
+    return render_template("index.html",
+                           title='ResNet50 Classification Demo')
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=PORT)
